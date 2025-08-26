f712875e9b983c545a40d562f9d69a5fcadf06dd
w-createira <w.ira.verbilovich@gmail.com>
2025-08-26 21:32:16 +0300

    feat: add heal player functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,25 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
+
+function endRound() {
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <=0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    } else if (currentPlayerHealth <=0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    } else  if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw!');
+    }
+}
 
 function attackMonster(mode) {
     let maxDamage;
@@ -36,5 +49,19 @@ function strongAttackHandler() {
     attackMonster('STRONG_ATTACK');
 }
 
+function healPlayerHandler() {
+    let healValue;
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+        alert("You cannot heal to more than your max initial health.");
+        healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+        healValue = HEAL_VALUE;
+    }
+    increasePlayerHealth(HEAL_VALUE);
+    currentPlayerHealth += HEAL_VALUE;
+    endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
