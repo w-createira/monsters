41304f8bef1b554c8a28bd42f2311cd78c3e2880
w-createira <w.ira.verbilovich@gmail.com>
2025-08-27 15:51:33 +0300

    refactor: rewrite functions using switch case and loops

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -28,46 +28,53 @@ adjustHealthBars(chosenMaxLife);
 
 function writeToLog(ev, val, monsterHealth, playerHealth) {
     let logEntry;
-    if (ev === LOG_EVENT_PLAYER_ATTACK) {
-        logEntry = {
-            event: ev,
-            value: val,
-            target: 'MONSTER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        };
-    } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-        logEntry = {
-            event: ev,
-            value: val,
-            target: 'MONSTER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        };
-    } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+    switch (ev) {
+        case LOG_EVENT_PLAYER_ATTACK: 
             logEntry = {
+                event: ev,
+                value: val,
+                target: 'MONSTER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            };
+        break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry = {
+                event: ev,
+                value: val,
+                target: 'MONSTER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            };    
+        break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry = {
+                event: ev,
+                value: val,
+                target: 'PLAYER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            };
+        break;
+        case LOG_EVENT_PLAYER_HEAL:
+                        logEntry = {
             event: ev,
             value: val,
             target: 'PLAYER',
             finalMonsterHealth: monsterHealth,
             finalPlayerHealth: playerHealth
         };
-    } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-            logEntry = {
-            event: ev,
-            value: val,
-            target: 'PLAYER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        };
-    } else if (ev === LOG_EVENT_GAME_OVER) {
-            logEntry = {
+        break;
+        case LOG_EVENT_GAME_OVER:
+                        logEntry = {
             event: ev,
             value: val,
             finalMonsterHealth: monsterHealth,
             finalPlayerHealth: playerHealth
         };
-        
+        break;
+        default: 
+            logEntry = {};
     }
     battleLog.push(logEntry);
 }
@@ -109,15 +116,8 @@ function endRound() {
 }
 
 function attackMonster(mode) {
-    let maxDamage;
-    let logEvent;
-    if (mode === MODE_ATTACK) {
-        maxDamage = ATTACK_VALUE;
-        logEvent = LOG_EVENT_PLAYER_ATTACK;
-    } else if (mode === MODE_STRONG_ATTACK) {
-        maxDamage = STRONG_ATTACK_VALUE;
-        logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
-    }
+    const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+    const logEvent = mode === MODE_ATTACK ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK;
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
     writeToLog(logEvent, damage, currentMonsterHealth, currentPlayerHealth);
@@ -147,7 +147,13 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-    console.log(battleLog);
+    for (const log of battleLog) {
+        console.log(`#${i}`);
+        for (const key in logEntry) {
+            console.log(`${key} => ${logEntry[key]}`)
+        }
+        i++;
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
